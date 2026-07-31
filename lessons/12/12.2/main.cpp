e2f06e86b089625d9e81e705409115f097d8b345
Akintayo Salu <saluakintayo@gmail.com>
2026-07-30 23:48:39 -0400

    07/30

---
+++ lessons/12/12.2/main.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+    int x{};
+    const double d{};
+
+    int y { x }; // x is a modifiable lvalue expression
+    const double e { d }; // d is a non-modifiable lvalue expression
+
+    return 0;
+}
